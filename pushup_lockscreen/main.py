227314d27ea332a52dc7617a53e1297dfc86ef75
Victor Sonck <victor.sonck@gmail.com>
2022-02-03 16:58:38 +0100

    fuckyeah dude, it just works now. Unlocking and all.

--- pushup_lockscreen/main.py
+++ pushup_lockscreen/main.py
@@ -46,11 +46,10 @@ class LockscreenClient:
 
     def lock(self):
         stdin, stdout, stderr = self.client.exec_command('DISPLAY=:0 i3lock &')
-        print(stderr.readlines())
 
     def unlock(self):
         stdin, stdout, stderr = self.client.exec_command('DISPLAY=:0 xdotool type <your_pc_password>')
-        print(stderr)
+        stdin, stdout, stderr = self.client.exec_command('DISPLAY=:0 xdotool key KP_Enter')
 
 
 class PushupLockscreen:
